f8f67c9b4fbfca3133b0cb9346b6f666ae98c6f0
activizator <activizator@ya.ru>
2021-03-20 11:22:12 +0300

    github api done

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { of, from } from 'rxjs';
+import { fromEvent } from 'rxjs';
+import { debounceTime, switchMap } from 'rxjs/operators'
 
-const o = of(5) // Promise.resolve(5)
+const myInput = document.querySelector('input');
 
-o.subscribe({
-  next: (value: any) => console.log('Next:', value),
-  complete: () => console.log('Complete!'),
-  error: (error) => console.log('Error!', error)
-})
+const o = fromEvent(myInput, 'input');
+
+o.pipe(switchMap(async (KeyboardEvent) => {
+  const res = await fetch(`https://api.github.com/search/repositories?q=${(KeyboardEvent.currentTarget as HTMLInputElement).value}`);
+  return res.json();
+}), debounceTime(1000))
+.subscribe(r => console.log(r.total_count));
